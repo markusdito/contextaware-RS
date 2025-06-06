47db3edf6bcf1d7e57f2479e77aa56943d845dd6
Markus Dito <markusdito@gmail.com>
2025-06-07 02:58:32 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import FashionGallery from './components/FashionGallery';
+
+function App() {
+    return (
+        <div className="App">
+            <h1 className="text-3xl font-bold text-center my-6">Fashion Product Gallery</h1>
+            <FashionGallery />
+        </div>
+    );
+}
+
+export default App;
