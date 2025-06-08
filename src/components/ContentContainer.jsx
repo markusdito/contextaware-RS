dc49e059d31721bff87574666faf3c7fac064d28
Markus Dito <markusdito@gmail.com>
2025-06-08 19:33:45 +0700

    reformat code

--- src/components/ContentContainer.jsx
+++ src/components/ContentContainer.jsx
@@ -7,12 +7,21 @@ const ContentContainer = ({products}) => {
         <div className="max-w-5xl mx-auto">
             <Jumbotron/>
             <ul className="flex gap-5">
-                <li><button className="text-2xl hover:text-blue-500">Women</button></li>
-                <li><button className="text-2xl hover:text-blue-500">Men</button></li>
-                <li><button className="text-2xl hover:text-blue-500">Boy</button></li>
-                <li><button className="text-2xl hover:text-blue-500">Girl</button></li>
+                <li>
+                    <button className="text-2xl hover:text-blue-500"
+                    >Women</button>
+                </li>
+                <li>
+                    <button className="text-2xl hover:text-blue-500">Men</button>
+                </li>
+                <li>
+                    <button className="text-2xl hover:text-blue-500">Boy</button>
+                </li>
+                <li>
+                    <button className="text-2xl hover:text-blue-500">Girl</button>
+                </li>
             </ul>
-            <ProductDispenser products={products} />
+            <ProductDispenser products={products}/>
         </div>
     )
 }
